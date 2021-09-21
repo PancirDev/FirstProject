394e1b22989162aac3a306d849fadcb67d5a4c8a
PancirDev <pancir.dev@gmail.com>
2021-09-21 21:43:05 +0300

    app customers's models

--- customers/models.py
+++ customers/models.py
@@ -1,2 +1,9 @@
 from django.db import models
 
+class Customers(models.Model):
+    company = models.CharField(max_length=150, unique=True)
+    name = models.CharField(max_length=150)
+    phonenumber = models.CharField(max_length=15)
+    email = models.EmailField()
+    address = models.CharField(max_length=300)
+    information = models.CharField(max_length=500)
